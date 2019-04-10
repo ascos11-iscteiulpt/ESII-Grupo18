6c3bfa9ab6ef8b789f117bddd39462c0ebb5f098
Bruno Colaço <barco@iscte-iul.pt>
2019-04-10 11:35:14 +0100

    Teste de commit

--- ES2/src/GUIAdministrador.java
+++ ES2/src/GUIAdministrador.java
@@ -19,7 +19,7 @@ public class GUIAdministrador {
 		JFrame guiFrame = new JFrame();
 		//make sure the program exits when the frame closes
 		guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		guiFrame.setTitle("GUI Teste");
+		guiFrame.setTitle("GUI este");
 		guiFrame.setSize(300,250);
 		//This will center the JFrame in the middle of the screen
 		guiFrame.setLocationRelativeTo(null);
